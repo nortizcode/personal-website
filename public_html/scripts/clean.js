24746913fff2e769e7628fb8e3c06fa2a29d8cfe
Nathan Ortiz <nkortiz92@gmail.com>
2020-06-10 13:19:37 -0600

    finished and deployed my website

--- public_html/scripts/clean.js
+++ public_html/scripts/clean.js
@@ -1,6 +1,6 @@
 const sh = require('shelljs');
 const upath = require('upath');
 
-const destPath = upath.resolve(upath.dirname(__filename), '../dist');
+const destPath = upath.resolve(upath.dirname(__filename), '../public_html');
 
 sh.rm('-rf', `${destPath}/*`)
